d7470a633da95cf9c62961a88f84e2d424551a19
Amaryo <1154318356@qq.com>
2025-12-27 16:15:19 +0800

    fix

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     BASE_URL = "https://personal-kd-njs.yun.139.com/hcy"
     DEV_INFO = "||9|7.14.0|chrome|120.0.0.0|||windows 10||zh-CN|||"
-    DEFAULT_MAX_WORKERS = 10
+    DEFAULT_MAX_WORKERS = 3
     DEFAULT_AUTHORIZATION = "" #改为你自己的令牌不需要复制basic
     DEFAULT_PARENT_ID = "/"  # 默认根目录ID
